6edd687f0c7bade1352bfef04e9f4ec80adbb953
fbplamachine <fouokajunior@gmail.com>
2023-02-28 08:00:59 +0100

    Implement a REST service for managing listings for online advertising service.
    Create a listing. All the created listings should have state draft by default;
    Update a listing;
    Get all listings of a dealer with a given state;
    Publish a listing;
    Unpublish a listing.

---
+++ src/test/java/com/fbplamachine/onlineAdvertisingService/OnlineAdvertisingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbplamachine.onlineAdvertisingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAdvertisingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
